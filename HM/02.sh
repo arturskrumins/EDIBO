8c822d9b886de69ecc6a299a6fc8c19a34d41775
arturskrumins <46200944+arturskrumins@users.noreply.github.com>
2020-07-07 18:15:27 +0300

    20200707 18:15
    papildinajums

--- HM/02.sh
+++ HM/02.sh
@@ -15,3 +15,29 @@ print $SECONDS
 
 done
 
+##########################
+
+
+#! /bin/bash
+
+if 
+test -f "02.dat";
+then
+    rm 02.dat
+    fi
+
+
+end=$((SECONDS+5))
+
+
+ 
+while [ $SECONDS -lt $end ]
+
+do
+  
+#df -h |egrep sda2 | awk '{printf $2/$3} '
+echo  $Seconds >> 02.dat 
+df / --output=pcent | tail -n 1 >> 02.dat;
+
+done
+
