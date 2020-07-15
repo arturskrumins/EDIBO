909fa6ed9fb67051643a7d9d4a3bd15bc014f890
arturskrumins <46200944+arturskrumins@users.noreply.github.com>
2020-07-15 18:48:30 +0300

    Add files via upload

---
+++ HM/Myvaluescontroller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyFirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("value")]
+    public class Myvaluescontroller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get(){
+           return new string []{"July"," 2020", "EDIBO","RTU"};
+        }
+       
+    }
+}
