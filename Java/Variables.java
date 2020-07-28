c681b70ae262ffb472c6af2a822ee6a274f7fd1d
user <notaufo@gmail.com>
2020-07-28 18:45:02 +0300

    20200728_18_44

---
+++ Java/Variables.java
@@ -0,0 +1,19 @@
+package java_basics;
+public class Variables{
+public static void main(String[] args){
+
+
+int b= 12;
+int c =23;
+System.out.println("Hello world "+b);
+System.out.println("Cik ir " +b +"+" +c  +"=" +(b+c));
+
+
+
+}
+
+
+
+
+
+}
